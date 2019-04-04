ecc0513d3e4edaaae7bec23f64a266af93c93a5f
Colby-Hall <hallcc@hendrix.edu>
2019-04-04 00:15:29 -0500

    Player sprite, animation

---
+++ IGD Lab Four/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpStrength;
+
+    public bool grounded;
+    public LayerMask checkGround;
+
+    private Rigidbody2D playerBody;
+    private Collider2D playerCollider;
+
+    private Animator playerAnimator;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+   
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(playerCollider, checkGround);
+        playerBody.velocity = new Vector2(moveSpeed, playerBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) 
+        {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, jumpStrength);
+        }
+    }
+}
